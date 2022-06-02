f448b3de1f2993fe7d1ed559728d0f6c2d3bb725
chanbito <69738195+chanbito@users.noreply.github.com>
2022-06-02 20:01:06 -0300

    Update dbo.Table.sql
    atualizando tabela faltante

--- MonitorDanfe/dbo.Table.sql
+++ MonitorDanfe/dbo.Table.sql
@@ -4,4 +4,14 @@
     [CaminhoEntradaUnimake] VARCHAR(MAX) NULL, 
     [Saida] VARCHAR(MAX) NULL, 
     [timer] INT NULL
+);
+
+CREATE TABLE [dbo].[Notas]
+(
+	[ID] INT NOT NULL PRIMARY KEY, 
+    [status] VARCHAR(MAX) NULL, 
+    [Caminho] VARCHAR(MAX) NULL, 
+    [Detalhes] VARCHAR(MAX) NULL, 
+    [Numero] VARCHAR(MAX) NULL, 
+    [DataProcess] Datetime NULL
 )
